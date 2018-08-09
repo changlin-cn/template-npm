99bd62123ace0825d743673588cd1aea0d3282c3
hechanglin <lin-cn@users.noreply.github.com>
2018-08-09 23:24:25 +0800

    Add devDependencies

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "commonjs": true,
+        "es6": true
+    },
+    globals:{
+
+    },
+    parser:'babel-eslint',
+    "extends": ["eslint:recommended"],
+    "parserOptions": {
+        "ecmaVersion": 2017,
+        "sourceType": "module"
+    },
+    "rules": {
+        'no-unused-vars':0,
+        'no-console':0,
+    }
+}
