979e2ced64e7c18838d7170923db118024b09230
hechanglin <lin-cn@users.noreply.github.com>
2018-11-20 23:10:30 +0800

    Update  default configuration

--- template-init.js
+++ template-init.js
@@ -6,7 +6,7 @@ const init = async function ({project,inquirer}) {
         {type: "input", name: "name", message: "package name: ", default: project},
         {type: "input", name: "version", message: "version: ", default: '0.0.0'},
         {type: "input", name: "description", message: "description: "},
-        {type: "input", name: "main", message: "entry point: ", default: 'index.js'},
+        {type: "input", name: "main", message: "entry point: ", default: 'dist/index.js'},
         {type: "input", name: "repository", message: "git repository: "},
         {type: "input", name: "keywords", message: "keywords: ", default: project},
         {type: "input", name: "author", message: "author: "},
